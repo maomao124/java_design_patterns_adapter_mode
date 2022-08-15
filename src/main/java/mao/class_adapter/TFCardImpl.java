cb74386c614cb20292c1b1f4c4e03e8d7b2cf319
mao <1296193245@qq.com>
2022-08-15 21:16:53 +0800

    java_design_patterns_adapter_mode

---
+++ src/main/java/mao/class_adapter/TFCardImpl.java
@@ -0,0 +1,30 @@
+package mao.class_adapter;
+
+/**
+ * Project name(项目名称)：java设计模式_适配器模式
+ * Package(包名): mao.class_adapter
+ * Class(类名): TFCardImpl
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/8/15
+ * Time(创建时间)： 21:16
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class TFCardImpl implements TFCard
+{
+
+    @Override
+    public String readTF()
+    {
+        return "读取TF卡";
+    }
+
+    @Override
+    public void writeTF(String msg)
+    {
+        System.out.println("写入TF卡：" + msg);
+    }
+}
